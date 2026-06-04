7404ad69bb27962338be159995840de82f8962f6
Leah Parparov <leahparparov@host-128-153-81-105.host.clarkson.edu>
2026-06-03 21:48:53 -0400

    deleted tester

--- network_dynamics/integration.py
+++ network_dynamics/integration.py
@@ -2,7 +2,6 @@
 Integrates one trajectory
 """
 import numpy as np
-import networkx as nx
 from scipy.integrate import solve_ivp
 from graphs import graph_laplacian
 from coupling import build_coupling_matrix
@@ -29,31 +28,3 @@ def integrate(G, initial_conditions, parameters, coupling_strength, H, tmax, tim
     return sol, sol_t
 
 
-def main():
-    print("Testing integration.py...")
-
-    G = nx.path_graph(5)
-
-    initial_conditions = np.arange(15, dtype = "float")
-
-    parameters = [0.2, 0.2, 7]
-
-    sol, t = integrate(
-        G=G,
-        initial_conditions=initial_conditions,
-        parameters=parameters,
-        coupling_strength=1,
-        H=None,
-        tmax=1,
-        timestep=0.05,
-    )
-
-    print("sol.shape:", sol.shape)
-    print("t.shape:", t.shape)
-    print("first state:", sol[0])
-    print("final state:", sol[-1])
-
-
-
-if __name__ == "__main__":
-    main()
