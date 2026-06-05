5e93f8b858c51fc609368dcfa51a1ddc4ed99a35
Leah <220226955+lparp06@users.noreply.github.com>
2026-06-04 22:08:53 -0400

    basin tester and store results from serial testing before i try to do cpu parallelism

--- network_dynamics/integration.py
+++ network_dynamics/integration.py
@@ -3,9 +3,9 @@ Integrates one trajectory
 """
 import numpy as np
 from scipy.integrate import solve_ivp
-from graphs import graph_laplacian
-from coupling import build_coupling_matrix
-from oscillators import rossler
+from .graphs import graph_laplacian
+from .coupling import build_coupling_matrix
+from .oscillators import rossler
 
 
 def integrate(G, initial_conditions, parameters, coupling_strength, H, tmax, timestep):
